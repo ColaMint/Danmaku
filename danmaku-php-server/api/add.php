2671a88a840de1ddb398554d9767f5bad2255e3c
xiaominghupan <463785757@qq.com>
2015-06-11 00:31:33 +0800

    完成API

---
+++ danmaku-php-server/api/add.php
@@ -0,0 +1,6 @@
+<?php
+	require_once(__dir__ . '/../common/init.php');
+    require_once(DMK_LIB_PATH . '/api_add.php');                                                                                                                                               
+    $api = new ApiAdd();
+    $api->run();    
+?> 
---
+++ danmaku-php-server/api/fetch.php
@@ -0,0 +1,8 @@
+<?php                                                                                                                                                                                                    
+ 	require_once(__dir__ . '/../common/init.php');
+	require_once(DMK_LIB_PATH . '/api_fetch.php');
+  
+  	$api = new ApiFetch();
+	$api->run();
+?>
+
