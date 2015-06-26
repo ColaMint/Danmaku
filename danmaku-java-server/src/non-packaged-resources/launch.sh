b5c1658bf8774545e96647db3618e3bec0fb869c
xiaominghupan <463785757@qq.com>
2015-06-26 22:47:48 +0800

    sh

--- danmaku-java-server/src/non-packaged-resources/launch.sh
+++ danmaku-java-server/src/non-packaged-resources/launch.sh
@@ -1,5 +1,5 @@
 #/usr/bin
 PROJECT_HOME=.
 MAIN_CLASS=com.danmaku.server.DanmakuServer
-LIB_OPTS="$PROJECT_HOME;$PROJECT_HOME/lib;$PROJECT_HOME/lib/*;$PROJECT_HOME/classes;"
+LIB_OPTS="$PROJECT_HOME:$PROJECT_HOME/lib:$PROJECT_HOME/lib/*:$PROJECT_HOME/classes"
 java -cp $LIB_OPTS $MAIN_CLASS
